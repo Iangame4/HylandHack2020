6d3889cd71938577bd6c3fc5424c2b0f78383097
Demure <dannynovak21@yahoo.com>
2020-01-04 00:14:41 -0500

    adding imageButtons to main menu

--- app/src/main/java/c/hylandhack/kintober/MainMenuActivity.kt
+++ app/src/main/java/c/hylandhack/kintober/MainMenuActivity.kt
@@ -2,11 +2,32 @@ package c.hylandhack.kintober
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main_menu.*
 
 class MainMenuActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+
+
+        var pic = true;
+        ibBiking.setOnClickListener {
+            ibBiking.setImageResource(
+                if(pic) R.mipmap.ic_launcher_round else R.mipmap.ic_launcher
+            )
+            pic = !pic
+        }
+
+        var pic2 = false;
+        ibWalking.setOnClickListener {
+            ibWalking.setImageResource(
+                if (pic2) R.mipmap.ic_walking_round else R.mipmap.ic_walking
+            )
+            pic2 = !pic2;
+        }
     }
 }
