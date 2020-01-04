f475e28e083f2835a8a56ced171ed39c64fee94d
Demure <dannynovak21@yahoo.com>
2020-01-04 15:38:03 -0500

    connecting the sbMiles to the etMiles, and setting up the cancel button

--- app/src/main/java/c/hylandhack/kintober/RandomActivity.kt
+++ app/src/main/java/c/hylandhack/kintober/RandomActivity.kt
@@ -1,14 +1,51 @@
 package c.hylandhack.kintober
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.SeekBar
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main_menu.*
+import kotlinx.android.synthetic.main.activity_random.*
+
 
 class RandomActivity : AppCompatActivity() {
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_random)
 
+        val etMiles = findViewById<EditText>(R.id.etMiles)
+        val sbMiles = findViewById<SeekBar>(R.id.sbMiles)
 
+        // Set a SeekBar change listener
+        sbMiles.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+
+            override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
+                // Display the current progress of SeekBar
+                etMiles.setText("$i")
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // Do something
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                // Do something
+
+            }
+        })
+
+        btnCancel.setOnClickListener {
+            val i = Intent(this, MainMenuActivity::class.java)
+            startActivity(i)
+        }
     }
 }
