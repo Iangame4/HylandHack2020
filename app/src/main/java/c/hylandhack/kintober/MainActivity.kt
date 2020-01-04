b2e7f81afedbd2482832d24c7815f297bd98d0c4
Iangame4 <ianpike98@gmail.com>
2020-01-03 22:14:28 -0500

    Added login

--- app/src/main/java/c/hylandhack/kintober/MainActivity.kt
+++ app/src/main/java/c/hylandhack/kintober/MainActivity.kt
@@ -4,16 +4,39 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.*
+import kotlinx.android.synthetic.main.activity_main.*
 
+/**
+ * Login Form
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    fun click(view: View){
-        val i = Intent(applicationContext, MapsActivity::class.java)
-        startActivity(i)
+
+        btnReset.setOnClickListener {
+            // clearing userName and password edit text views on reset button click
+            etUserName.setText("")
+            etPassword.setText("")
+        }
+
+        // set on-click listener
+        btnSubmit.setOnClickListener {
+            val userName = etUserName.text;
+            val password = etPassword.text;
+            Toast.makeText(this@MainActivity, userName, Toast.LENGTH_LONG).show()
+
+            // your code to validate the userName and password combination
+            // and verify the same
+
+            val i = Intent(this, MapsActivity::class.java)
+            startActivity(i)
+
+
+
+        }
     }
 }
