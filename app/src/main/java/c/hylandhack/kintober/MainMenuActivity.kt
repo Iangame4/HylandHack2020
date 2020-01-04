dce9c724a37c8c8fa8497faeea26859647ec3346
Demure <dannynovak21@yahoo.com>
2020-01-04 04:03:35 -0500

    designing random track ui

--- app/src/main/java/c/hylandhack/kintober/MainMenuActivity.kt
+++ app/src/main/java/c/hylandhack/kintober/MainMenuActivity.kt
@@ -1,5 +1,6 @@
 package c.hylandhack.kintober
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -33,11 +34,53 @@ class MainMenuActivity : AppCompatActivity() {
                 }
                 else R.mipmap.ic_walking
             )
+            ibSpeedRun.setImageResource(
+                if (pic)
+                {
+
+                    R.mipmap.ic_speedrun
+
+                }
+                else R.mipmap.ic_speedrun
+            )
+
             pic = !pic
         }
 
-        //changes the walking image of the image button
+        //changes the biking image of the image button
+        ibSpeedRun.setOnClickListener {
+            ibSpeedRun.setImageResource(
+                if(pic)
+                {
 
+                    R.mipmap.ic_launcher_round
+                }
+                else R.mipmap.ic_launcher_round
+            )
+            ibBiking.setImageResource(
+                if (pic)
+                {
+
+                    R.mipmap.ic_speedrun
+
+                }
+                else R.mipmap.ic_speedrun
+            )
+            ibWalking.setImageResource(
+                if (pic)
+                {
+
+                    R.mipmap.ic_walking
+
+                }
+                else R.mipmap.ic_walking
+            )
+
+            pic = !pic
+        }
+
+
+        //changes the walking image of the image button
         ibWalking.setOnClickListener {
             ibWalking.setImageResource(
                 if (pic)
@@ -55,8 +98,20 @@ class MainMenuActivity : AppCompatActivity() {
                 }
                 else R.mipmap.ic_walking
             )
+            ibSpeedRun.setImageResource(
+                if (pic)
+                {
+
+                    R.mipmap.ic_speedrun
+
+                }
+                else R.mipmap.ic_speedrun
+            )
             pic = !pic;
         }
-
+            btnRandom.setOnClickListener {
+            val i = Intent(this, RandomActivity::class.java)
+            startActivity(i)
+        }
     }
 }
